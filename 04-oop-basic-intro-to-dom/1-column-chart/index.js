5596fbca604afe23fade8b8757d96ac3b7d98d2a
SitnikovYuriy <sity@inbox.ru>
2021-02-09 10:55:08 +0300

    04 fixed

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -11,47 +11,39 @@ export default class ColumnChart {
   render() {
     const wrapper = document.createElement('div');
     wrapper.innerHTML = `
-      <div class="column-chart">
-        <div class="column-chart__title"></div>
+      <div class="column-chart" style="--chart-height: ${ this.chartHeight }">
+        <div class="column-chart__title">
+          ${ this.label }
+          ${ this.link ? `<a href="/${ this.link }" class="column-chart__link">View all</a>` : '' }
+        </div>
         <div class="column-chart__container">
-          <div data-element="header" class="column-chart__header"></div>
+          <div data-element="header" class="column-chart__header">${ this.value }</div>
           <div data-element="body" class="column-chart__chart"></div>
         </div>
       </div>
     `;
     this.element = wrapper.firstElementChild;
-    this.renderHeight();
-    this.renderTitle();
-    this.renderHeader();
     this.renderData();
-  }
-
-  renderHeight() {
-    this.element.style['--chart-height'] = this.chartHeight;
-  }
-
-  renderTitle() {
-    this.element.querySelector('.column-chart__title').innerHTML =
-      this.link ? `${ this.label }<a href="/${ this.link }" class="column-chart__link">View all</a>` : this.label;
-  }
-
-  renderHeader() {
-    this.element.querySelector('.column-chart__header').innerHTML = this.value;
   }
 
   renderData() {
     const body = this.element.querySelector('.column-chart__chart');
     if (this.data.length) {
-      const max = Math.max(...this.data);
-      const factor = this.chartHeight / max;
-      body.innerHTML = this.data.map(item => `
-        <div style="--value: ${ Math.floor(item * factor) }" data-tooltip="${ (item / max * 100).toFixed(0) }%"></div>
-      `).join('');
+      body.innerHTML = this.getDataItems().join('');
       this.element.classList.remove('column-chart_loading');
     } else {
-      this.element.classList.add('column-chart_loading');
       body.innerHTML = '';
+      this.element.classList.add('column-chart_loading');
     }
+  }
+
+  getDataItems() {
+    const max = Math.max(...this.data);
+    return this.data.map(item => {
+      const value = Math.floor(item / max * this.chartHeight);
+      const tooltip = (item / max * 100).toFixed(0) + '%';
+      return `<div style="--value: ${ value }" data-tooltip="${ tooltip }"></div>`;
+    });
   }
 
   update(data = []) {
