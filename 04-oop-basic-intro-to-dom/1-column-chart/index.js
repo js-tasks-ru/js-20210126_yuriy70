fc8dd539f6348ccfee9f08525b590f908a49b045
SitnikovYuriy <sity@inbox.ru>
2021-02-10 10:45:22 +0300

    04 fixed+

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,54 +1,66 @@
 export default class ColumnChart {
-  constructor({ data = [], label = '', value = 0, link = '', chartHeight = 50 } = {}) {
+  subElements = {};
+  chartHeight = 50;
+
+  constructor({
+    data = [],
+    label = '',
+    value = 0,
+    link = '',
+  } = {}) {
     this.data = data;
     this.label = label;
     this.value = value;
     this.link = link;
-    this.chartHeight = chartHeight;
+    
     this.render();
+  }
+
+  get template() {
+    return `
+      <div class="column-chart column-chart_loading" style="--chart-height: ${this.chartHeight}">
+        <div class="column-chart__title">
+          ${this.label}
+          ${this.link ? `<a href="/${this.link}" class="column-chart__link">View all</a>` : ''}
+        </div>
+        <div class="column-chart__container">
+          <div data-element="header" class="column-chart__header">${this.value}</div>
+          <div data-element="body" class="column-chart__chart">${this.body}</div>
+        </div>
+      </div>
+    `;
+  }
+
+  get body() {
+    const max = Math.max(...this.data);
+    return this.data.map(item => `
+      <div style="--value: ${Math.floor(item / max * this.chartHeight)}" data-tooltip="${(item / max * 100).toFixed(0)}%"></div>
+    `).join('');
   }
 
   render() {
     const wrapper = document.createElement('div');
-    wrapper.innerHTML = `
-      <div class="column-chart" style="--chart-height: ${ this.chartHeight }">
-        <div class="column-chart__title">
-          ${ this.label }
-          ${ this.link ? `<a href="/${ this.link }" class="column-chart__link">View all</a>` : '' }
-        </div>
-        <div class="column-chart__container">
-          <div data-element="header" class="column-chart__header">${ this.value }</div>
-          <div data-element="body" class="column-chart__chart"></div>
-        </div>
-      </div>
-    `;
+    wrapper.innerHTML = this.template;
     this.element = wrapper.firstElementChild;
-    this.renderData();
-  }
 
-  renderData() {
-    const body = this.element.querySelector('.column-chart__chart');
     if (this.data.length) {
-      body.innerHTML = this.getDataItems().join('');
       this.element.classList.remove('column-chart_loading');
-    } else {
-      body.innerHTML = '';
-      this.element.classList.add('column-chart_loading');
     }
+
+    this.subElements = this.getSubElements();
   }
 
-  getDataItems() {
-    const max = Math.max(...this.data);
-    return this.data.map(item => {
-      const value = Math.floor(item / max * this.chartHeight);
-      const tooltip = (item / max * 100).toFixed(0) + '%';
-      return `<div style="--value: ${ value }" data-tooltip="${ tooltip }"></div>`;
-    });
+  getSubElements() {
+    const elements = this.element.querySelectorAll('[data-element]');
+    return [...elements].reduce((accum, subElement) => {
+      accum[subElement.dataset.element] = subElement;
+      return accum;
+    }, {});
   }
 
   update(data = []) {
     this.data = data;
-    this.renderData();
+    this.subElements.body.innerHTML = this.body;
   }
 
   remove() {
